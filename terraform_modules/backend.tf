5d3b9624bbf5dc6d3ab66ad404b84335bf9504b1
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2025-02-14 00:24:26 +0200

    change S3 bucket's name

--- terraform_modules/backend.tf
+++ terraform_modules/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
    region = "eu-north-1"
-   bucket = var.terraform-remote-state
+   bucket = var.terraform-remote-state-devopsguy-001
    key = "statefilebucket"
   }
 }
