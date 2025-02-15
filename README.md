b99828e91eb267a8a4ae0158e4fa40aa465ac2cd
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2025-02-15 22:26:10 +0200

    change inline ingress and egress rules

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # property_management_system_infrastructure
 Infrastructure repo for provisioning and configuring a Kubernetes cluster on AWS using Terraform and Ansible. 
+
+Unlike S3, you don’t create EC2 instances manually if they’re defined in your Terraform configuration. Terraform will automatically create them when you run terraform apply.
---
+++ terraform_modules/securitygroup.tf
@@ -0,0 +1,69 @@
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.kubernetes_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_k8s_api" {
+  security_group_id = aws_security_group.kubernetes_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 6443
+  to_port           = 6443
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_etcd" {
+  security_group_id = aws_security_group.kubernetes_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 2379
+  to_port           = 2380
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_kubelet" {
+  security_group_id = aws_security_group.kubernetes_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 10250
+  to_port           = 10252
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_nodeport" {
+  security_group_id = aws_security_group.kubernetes_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 30000
+  to_port           = 32767
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.kubernetes_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 8080
+  to_port           = 8080
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_custom_port" {
+  security_group_id = aws_security_group.kubernetes_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 6109
+  to_port           = 6109
+  ip_protocol       = "tcp"
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_icmp" {
+  security_group_id = aws_security_group.kubernetes_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = -1
+  to_port           = -1
+  ip_protocol       = "icmp"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_egress" {
+  security_group_id = aws_security_group.kubernetes_sg.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1"  # -1 means all protocols (i.e., all ports)
+}
