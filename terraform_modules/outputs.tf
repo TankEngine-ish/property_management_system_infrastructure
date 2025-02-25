27223b08944cbf3e8b8cdfe71d42258730a728cc
TankEngine-ish <131184681+TankEngine-ish@users.noreply.github.com>
2025-02-25 22:57:11 +0200

    create an outputs file to get EC2s IP addresses

---
+++ terraform_modules/outputs.tf
@@ -0,0 +1,19 @@
+output "master-node-ip" {
+  value       = aws_instance.master_node.private_ip
+  description = "The private IP of the master node"
+}
+
+output "worker-node-ip" {
+  value       = aws_instance.worker_node.private_ip
+  description = "The private IP of the worker node"
+}
+
+output "haproxy-server-ip" {
+  value       = aws_instance.haproxy_server.public_ip
+  description = "The public IP of the HAProxy server"
+}
+
+output "openvpn-server-ip" {
+  value       = aws_instance.openvpn_server.public_ip
+  description = "The public IP of the OpenVPN server"
+}
